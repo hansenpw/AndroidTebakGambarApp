ec535cd8bc6848e61b108783635c38f191a74586
Hansen <leohansenpw@gmail.com>
2017-08-06 13:37:05 +0700

    add description

--- app/src/main/java/com/project/hansen/tebakgambartrial/MainActivity.java
+++ app/src/main/java/com/project/hansen/tebakgambartrial/MainActivity.java
@@ -1,8 +1,8 @@
 package com.project.hansen.tebakgambartrial;
 
 import android.content.SharedPreferences;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,26 +14,35 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
-    String jawaban;
-    ImageView gambar;
-    EditText teks;
-    Button jawab;
-    TextView skor;
-    int j, k;
-    SharedPreferences saveskor;
-    int currentscore = 0;
+    private final String key = "score";
+    private String jawaban;
+    private ImageView gambar;
+    private EditText teks;
+    private TextView skor;
+    private int j, k;
+    private SharedPreferences saveskor;
+    private int currentscore = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // set up views
         gambar = (ImageView) findViewById(R.id.gambar);
         teks = (EditText) findViewById(R.id.teks);
-        jawab = (Button) findViewById(R.id.jawab);
+        Button jawab = (Button) findViewById(R.id.jawab);
         skor = (TextView) findViewById(R.id.skor);
+
+        /**
+         * Set up SharedPreferences to save highscore
+         * @see android.content.SharedPreferences
+         */
         saveskor = getSharedPreferences("saveskor", 0);
-        final int score = saveskor.getInt("score", 0);
+        final int score = saveskor.getInt(key, 0);
         skor.setText("Highscore: " + score);
+
+        // start tebak gambar session
         j = CreateQuestion();
         k = j;
         jawab.setOnClickListener(new View.OnClickListener() {
@@ -42,14 +51,16 @@ public class MainActivity extends AppCompatActivity {
                 String input = teks.getText().toString();
                 if (input.equalsIgnoreCase(jawaban)) {
                     Toast.makeText(MainActivity.this, "Jawaban anda benar YEAY!!!", Toast.LENGTH_LONG).show();
+                    // loop to make sure the same question doesn't show up when randomizing question
                     while (k == j) {
                         j = CreateQuestion();
                     }
                     currentscore++;
-                    if (currentscore > saveskor.getInt("score", 0)) {
+                    // save to SharedPreferences each time highscore changes
+                    if (currentscore > saveskor.getInt(key, 0)) {
                         SharedPreferences.Editor editor = saveskor.edit();
-                        editor.putInt("score", currentscore);
-                        editor.commit();
+                        editor.putInt(key, currentscore);
+                        editor.apply();
                         skor.setText("Highscore: " + currentscore);
                     }
                 } else {
@@ -59,6 +70,11 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    /**
+     * Create Question by randomizing 3 questions
+     *
+     * @return id of the selected randomize question
+     */
     public int CreateQuestion() {
         Random random = new Random();
         int i = random.nextInt(3);
